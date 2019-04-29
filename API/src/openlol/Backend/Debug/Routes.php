4bd717b1dee8a5ec561bf509e3d3b1993d7b79d2
TheRealTeunissenStefan <grijsje@hotmail.com>
2019-04-29 19:55:11 +0200

    Using php-enum in VoteType for simplicity

--- API/src/openlol/Backend/Debug/Routes.php
+++ API/src/openlol/Backend/Debug/Routes.php
@@ -2,34 +2,43 @@
 namespace Openlol\Backend\Debug
 {
 
-	use Slim\App;
-	use Slim\Http\Request;
-	use Slim\Http\Response;
+    use Openlol\Model\VoteType;
+    use Slim\App;
+    use Slim\Http\Request;
+    use Slim\Http\Response;
 
 
-	class Routes
-	{
-		static function getRoutes(App $app)
-		{
-			$container = $app->getContainer();
+    class Routes
+    {
+        static function getRoutes(App $app)
+        {
+            $container = $app->getContainer();
 
-			$app->group('/debug', function (App $app) use ($container)
-			{
-				$container->get('logger')->info("Debug route '/debug' accessed");
+            $app->group('/debug', function (App $app) use ($container)
+            {
+                $container->get('logger')->info("Debug route '/debug' accessed");
 
-				$app->group('/user', function (App $app) use ($container)
-				{
-					$container->get('logger')->info("Debug route '/debug/user' accessed");
+                $app->group('/user', function (App $app) use ($container)
+                {
+                    $container->get('logger')->info("Debug route '/debug/user' accessed");
 
-					$app->get('/{id:[0-9]+}', function (Request $request, Response $response, array $args)
-					{
-						return $response->withJson(array($args));
+                    $app->get('/{id:[0-9]+}', function (Request $request, Response $response, array $args)
+                    {
+                        return $response->withJson(array($args));
 
-					});
+                    });
 
-				});
-			});
+                });
 
-		}
-	}
+                $app->get('/phpenum', function (Request $request, Response $response, array $args)
+                {
+                    return $response->withJson([
+                        'upvote'    => VoteType::Upvote(),
+                        'downvote'  => VoteType::Downvote()
+                    ]);
+                });
+            });
+
+        }
+    }
 }
